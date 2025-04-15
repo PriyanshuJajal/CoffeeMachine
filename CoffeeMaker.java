5220b36e332dca11d867c75feac7f5f3b8aaef45
Priyanshu Jajal <jajal.priyanshu07@gmail.com>
2025-04-15 10:24:46 +0530

    Calling the 2 methods

---
+++ CoffeeMaker.java
@@ -0,0 +1,11 @@
+public class CoffeeMaker {
+
+    public void prepareCoffee(Coffee myFavoriteCoffee) {
+
+        // Call the grindBeans() method on the Coffee object
+        myFavoriteCoffee.grindBeans();
+
+        // Call the brewCoffee() method on the Coffee object
+        myFavoriteCoffee.brewCoffee();
+    }
+}
